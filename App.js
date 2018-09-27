d636bc51f96e82f7b0b80e9617f485f3fc48380d
Summerly <Summerly.Xi@gmail.com>
2018-09-27 16:58:47 +0800

    Get started to use TextInput

--- App.js
+++ App.js
@@ -7,14 +7,14 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, TextInput} from 'react-native';
 
 type Props = {};
 
 class Greeting extends Component {
   render() {
     return (
-      <Text>Hello {this.props.name}</Text>
+      <Text style={styles.red}>Hello {this.props.name}</Text>
     )
   }
 }
@@ -39,6 +39,28 @@ class Blink extends Component {
   };
 }
 
+class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder='Type here to translate!'
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join('')}
+        </Text>
+      </View>
+    );
+  }
+}
+
 export default class App extends Component<Props> {
   render() {
     let pic = {
@@ -47,10 +69,21 @@ export default class App extends Component<Props> {
 
     return (
       <View style={styles.container}>
-        <Text style={styles.instructions}>Hello world!</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Greeting name='Native'/>
-        <Blink text='I love to blink'/>
+        <View style={{width: 200, height: 50, backgroundColor: 'powderblue'}}>
+          <Text style={styles.instructions}>Hello world!</Text>
+        </View>
+        <View style={{width: 200, height: 50, backgroundColor: 'steelblue'}}>
+          <Greeting name='Native'/>
+        </View>
+        <View style={{width: 200, height: 50, backgroundColor: 'skyblue'}}>
+          <Blink text='I love to blink'/>
+        </View>
+        <View style={{width: 200, height: 50}}>
+          <Image source={pic} style={{width: 200, height: 50}}/>
+        </View>
+        <View style={{width: 200, height: 50}}>
+          <PizzaTranslator/>
+        </View>
       </View>
     );
   }
@@ -68,4 +101,7 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  red: {
+    color: 'red'
+  }
 });
