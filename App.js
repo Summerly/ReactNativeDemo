e60cc1abb552555680b51f5bb9c29097ba6d1ba5
Summerly <Summerly.Xi@gmail.com>
2018-09-27 13:50:47 +0800

    Get started to use state and props

--- App.js
+++ App.js
@@ -7,23 +7,50 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 type Props = {};
-export default class App extends Component<Props> {
+
+class Greeting extends Component {
   render() {
     return (
+      <Text>Hello {this.props.name}</Text>
+    )
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {isShowingText: true};
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return {isShowingText: !previousState.isShowingText}
+      })
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : '';
+    return (
+      <Text>{display}</Text>
+    )
+  };
+}
+
+export default class App extends Component<Props> {
+  render() {
+    let pic = {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
+
+    return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>Hello world!</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Greeting name='Native'/>
+        <Blink text='I love to blink'/>
       </View>
     );
   }
@@ -35,11 +62,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
   instructions: {
     textAlign: 'center',
